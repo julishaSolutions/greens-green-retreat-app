d7dac36ef3945e0684adaf13dd00da41ee6f9d5a
Julisha Photography <julishaphotography@gmail.com>
2025-07-08 16:23:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tigoni.life',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   allowedDevOrigins: [
