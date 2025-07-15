3ab115f225461919394185715ad2ab47abf7d237
Julisha Photography <julishaphotography@gmail.com>
2025-07-15 11:27:13 +0000

    We have an issue: '''' studio-831432302:~/studio{dev}$ git add . git com

--- dev.nix
+++ dev.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }: {
-  # To learn more about flakes and dev environments, visit https://nix.dev/
   packages = [
-    # For Next.js development
-    pkgs.nodejs_20
-    # For using git with SSH authentication
     pkgs.openssh
+    pkgs.git-filter-repo
   ];
-  preCommit.tools = {
-    prettier = {
-      enable = true;
-      package = pkgs.nodePackages.prettier;
-    };
-  };
 }
